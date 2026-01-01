ce0ebd34f48b569c8244e3b88bfd8efc0876f0e5
loskyertt <loskyertt0403@gmail.com>
2026-01-01 18:02:26 +0800

    4th commit: correct flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,17 @@
 {
   description = "Personal dev templates";
 
-  outputs = _: {
+  outputs = { self, ... }: {        # <-- 这里把 self 留下来
     templates = {
-      default = self.templates.c-cpp;   # 默认模板
-
       c-cpp = {
-        path    = ./c-cpp;
+        path        = ./c-cpp;
         description = "C/C++ dev-shell with cmake, clangd, gdb";
       };
-
       node-js = {
-        path    = ./node-js;
+        path        = ./node-js;
         description = "Node.js/typescript workspace";
       };
+      default = self.templates.c-cpp;   # 现在 self 可见
     };
   };
 }
